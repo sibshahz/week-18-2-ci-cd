36f080a7964820030467ab915aebea21e90d8d49
Shahid Gillani <shahid5ssg@gmail.com>
2024-07-17 19:11:53 +0500

    testing ci/cd

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -18,7 +18,7 @@ export const Appbar = ({
         <div className="text-lg flex flex-col justify-center">
             PayTM
         </div>
-        <div className="flex flex-col justify-center pt-2 new-class">
+        <div className="flex flex-col justify-center pt-2 new-class updates">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
     </div>
